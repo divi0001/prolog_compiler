d48ee4e40aff7d07067475b2b3f7b041eb7d9bd6
BuildTools <unconfigured@null.spigotmc.org>
2023-01-28 17:15:52 +0100

    as promised this time sld is not commented

--- deklprog-project/src/SLD.hs
+++ deklprog-project/src/SLD.hs
@@ -20,23 +20,23 @@ import Data.List
 data SLDTree = SLDTree Goal [(Subst, SLDTree)]
   deriving (Show)
 
--- sld :: Prog -> Goal -> SLDTree
--- sld (Prog []) g = SLDTree g []
--- sld p (Goal []) = SLDTree (Goal []) []
--- sld p g = sldHelp p g [] 
+sld :: Prog -> Goal -> SLDTree
+sld (Prog []) g = SLDTree g []
+sld p (Goal []) = SLDTree (Goal []) []
+sld p g = sldHelp p g [] 
 
--- sldHelp :: Prog -> Goal -> [VarName] -> SLDTree
--- sldHelp (Prog []) g vars = SLDTree g []
--- sldHelp p (Goal []) vars = SLDTree (Goal []) []
--- sldHelp (Prog [Rule r rs]) (Goal t) = if isJust (unify r t) then SLDTree (Goal []) [(mkUni(unify r t)), sldHelp ]
--- sldHelp (Prog ((Rule r rs):rx)) (Goal (t:ts)) vars =  if isJust (unify r t)
---                                                         then SLDTree (Goal ts) [(mkUni(unify r t), sldHelp (Prog (renameList vars rx)) (Goal ts) (vars ++ allVars (Rule r rs)))] 
---                                                         else sld (Prog rx) (Goal (t:ts))
+sldHelp :: Prog -> Goal -> [VarName] -> SLDTree
+sldHelp (Prog []) g vars = SLDTree g []
+sldHelp p (Goal []) vars = SLDTree (Goal []) []
+sldHelp (Prog [Rule r rs]) (Goal [t]) vars = if isJust (unify r t) then SLDTree (Goal []) [(mkUni(unify r t), sldHelp (Prog []) (Goal []) vars)] else SLDTree (Goal [t]) []
+sldHelp (Prog ((Rule r rs):rx)) (Goal (t:ts)) vars =  if isJust (unify r t)
+                                                        then SLDTree (Goal ts) [(mkUni(unify r t), sldHelp (Prog (renameList vars rx)) (Goal ts) (vars ++ allVars (Rule r rs)))] 
+                                                        else sld (Prog rx) (Goal (t:ts))
 
 
--- renameList :: [VarName] -> [Rule] -> [Rule]
--- renameList vars rs = map (rename (nub(vars ++ concatMap (\r -> allVars r) rs))) rs
+renameList :: [VarName] -> [Rule] -> [Rule]
+renameList vars rs = map (rename (nub(vars ++ concatMap (\r -> allVars r) rs))) rs
 
--- mkUni :: Maybe Subst -> Subst
--- mkUni (Just s1) = s1
--- mkUni Nothing = empty
+mkUni :: Maybe Subst -> Subst
+mkUni (Just s1) = s1
+mkUni Nothing = empty
