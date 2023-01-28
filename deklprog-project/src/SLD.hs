9dd9a40c07f75f9dd59a973c12d1454e35afe90d
BuildTools <unconfigured@null.spigotmc.org>
2023-01-28 15:28:08 +0100

    Unification.hs ist fertig, sld auskommentiert (kommt nächste Version wieder rein)

--- deklprog-project/src/SLD.hs
+++ deklprog-project/src/SLD.hs
@@ -19,17 +19,17 @@ import Vars
 data SLDTree = SLDTree Goal [(Subst, SLDTree)]
   deriving (Show)
 
-sld :: Prog -> Goal -> SLDTree
-sld (Prog []) g = SLDTree g []
-sld p (Goal []) = SLDTree (Goal []) []
-sld (Prog ((Rule r rs):rx) ) (Goal (t:ts)) = if isJust (unify r t) 
-                                             then SLDTree (Goal ts) [(mkUni(unify r t), sld (Prog ((Rule (rename (map allVars r ++ foldl (\ rr -> allVars rr) rs) r) 
-                                             map ((\rrr -> rename (map allVars r ++ foldl (\ rr -> allVars rr) rs) ))rs):rx)) (Goal ts) )] 
-                                             else sld (Prog rx) (Goal (t:ts))
+-- sld :: Prog -> Goal -> SLDTree
+-- sld (Prog []) g = SLDTree g []
+-- sld p (Goal []) = SLDTree (Goal []) []
+-- sld (Prog ((Rule r rs):rx) ) (Goal (t:ts)) = if isJust (unify r t) 
+--                                              then SLDTree (Goal ts) [(mkUni(unify r t), sld (Prog ((Rule (rename (map allVars r ++ foldl (\ rr -> allVars rr) rs) r) 
+--                                              map ((\rrr -> rename (map allVars r ++ foldl (\ rr -> allVars rr) rs) ))rs):rx)) (Goal ts) )] 
+--                                              else sld (Prog rx) (Goal (t:ts))
 
 
 
 
-mkUni :: Maybe Subst -> Subst
-mkUni (Just s1) = s1
-mkUni Nothing = empty
+-- mkUni :: Maybe Subst -> Subst
+-- mkUni (Just s1) = s1
+-- mkUni Nothing = empty
--- deklprog-project/src/Unification.hs
+++ deklprog-project/src/Unification.hs
@@ -59,30 +59,116 @@ ds t1 (Comb st2 []) = if t1 /= Comb st2 [] then Just (t1, Comb st2 []) else Noth
 ds (Comb st1 (x:xs)) (Comb st2 (y:ys)) = if ds x y /= Nothing && x /= y then Just (x, y) else if x == y then Nothing else ds (Comb st1 xs) (Comb st2 ys)
 -}
 
-ds :: Term -> Term -> Maybe (Term, Term)
-ds (Var x) (Var y)                        = if x /= y then Just (Var x, Var y) else Nothing
-ds (Var x) (Comb name term)               = Just (Var x, Comb name term)
-ds (Comb name term) (Var x)               = Just (Var x, Comb name term)
-ds (Comb name1 term1) (Comb name2 term2)  = if name1 == name2 && term1 == term2 then Nothing else Just (Comb name1 term1, Comb name2 term2)
+-- ds :: Term -> Term -> Maybe (Term, Term)
+-- ds (Var x) (Var y)                        = if x /= y then Just (Var x, Var y) else Nothing
+-- ds (Var x) (Comb name term)               = Just (Var x, Comb name term)
+-- ds (Comb name term) (Var x)               = Just (Var x, Comb name term)
+-- ds (Comb name1 term1) (Comb name2 term2)  = if name1 == name2 && term1 == term2 then Nothing else Just (Comb name1 term1, Comb name2 term2)
 
+-- unify :: Term -> Term -> Maybe Subst
+-- unify t11 t21 = h1 t11 t21 (ds t11 t21) empty 
+--   where
+--     h1 :: Term -> Term -> Maybe (Term, Term) -> Subst -> Maybe Subst
+--     h1 (Var x) (Var y) (Just (t3, t4)) sub = if t3 /= t4 then Just (compose sub (single x (Var y))) else Nothing
+--     h1 (Comb st1 t1) (Var y) (Just (t3,t4)) sub = if y `elem` allVars (Comb st1 t1) then Nothing else Just (compose sub (single y (Comb st1 t1)))
+--     h1 (Var x) (Comb st2 t2) (Just (t3,t4)) sub = if x `elem` allVars (Comb st2 t2) then Nothing else Just (compose sub (single x (Comb st2 t2)))
+--     h1 (Comb st1 []) (Comb st2 t2) _ _ = Nothing
+--     h1 (Comb st1 t1) (Comb st2 []) _ _ = Nothing
+--     h1 (Comb st1 (t1:tx)) (Comb st2 (t2:ty)) (Just (t3, t4)) sub = if st1 /= st2 then Nothing else if (ds t1 t2 /= Nothing) then Just (compose sub (compose (notMaybe(h1 t1 t2 (ds t1 t2) sub))
+--                                                                    (notMaybe(h1 (Comb st1 tx) (Comb st2 ty) (ds (head tx) (head ty)) sub)) ) ) else Nothing
+--     h1 _ _ _ _ = Nothing
+--     notMaybe :: Maybe Subst -> Subst
+--     notMaybe (Just s1) = s1
+--     notMaybe Nothing = empty
+
+
+
+
+
+
+
+ds :: Term -> Term -> Maybe (Term, Term)
+ds (Var x) (Var y) = if x == y then Nothing else Just (Var x, Var y)
+ds (Var x) (Comb st2 t2) = if occursfold x t2 then Nothing else Just (Var x, Comb st2 t2)
+ds (Comb st1 t1) (Var y) = if occursfold y t1 then Nothing else Just (Var y, Comb st1 t1)
+ds (Comb st1 t1) (Comb st2 t2)
+  | st1 /= st2 || numOfTerms t1 /= numOfTerms t2 = Just (Comb st1 t1, Comb st2 t2)
+  | st1 == st2
+      &&
+        numOfTerms t1 == numOfTerms t2 && fst (areSomeTermsNotEqual t1 t2 0)
+  = Just (t1 !! snd (areSomeTermsNotEqual t1 t2 0), t2 !! snd (areSomeTermsNotEqual t1 t2 0))
+  | otherwise = Nothing
+
+numOfTerms :: [Term] -> Int
+numOfTerms [] = 0
+numOfTerms (_:ts) = 1 + numOfTerms ts
+
+
+occursfold :: VarName -> [Term] -> Bool
+occursfold x1 [] = False
+occursfold x1 [t] = occurs x1 t
+occursfold x1 (t:ts) = occurs x1 t && occursfold x1 ts
+
+
+areSomeTermsNotEqual :: [Term] -> [Term] -> Int -> (Bool,Int)
+areSomeTermsNotEqual [] t2 i = if t2 /= [] then (True, i) else (False,i)
+areSomeTermsNotEqual t1 [] i = if t1 /= [] then (True, i) else (False,i)
+areSomeTermsNotEqual [Var x] [Var y] i = ((x /= y), i)
+areSomeTermsNotEqual [Comb st1 t1] [Var y] i = (True, i)
+areSomeTermsNotEqual [Var x] [Comb st2 t2] i = (True, i)
+areSomeTermsNotEqual [Comb st1 t1] [Comb st2 t2] i = ((st1 /= st2) && fst (areSomeTermsNotEqual t1 t2 (i+1)), snd (areSomeTermsNotEqual t1 t2 (i+1)))
+areSomeTermsNotEqual ((Var x):ts1) ((Var y):ts2) i = ((x /= y) && fst (areSomeTermsNotEqual ts1 ts2 (i+1)), snd (areSomeTermsNotEqual ts1 ts2 (i+1)))
+areSomeTermsNotEqual ((Var x):ts1) ((Comb st2 t2):ts2) i = (False, i+1)
+areSomeTermsNotEqual ((Comb st1 t1):ts1) ((Var y):ts2) i = (False, i+1)
+areSomeTermsNotEqual ((Comb st1 t1):ts1) ((Comb st2 t2):ts2) i = ((st1 /= st2) && fst (areSomeTermsNotEqual t1 t2 (i+1)) && fst (areSomeTermsNotEqual ts1 ts2 (snd (areSomeTermsNotEqual t1 t2 (i+1))))
+  , i + snd (areSomeTermsNotEqual t1 t2 (i+1)) + snd (areSomeTermsNotEqual ts1 ts2 (snd (areSomeTermsNotEqual t1 t2 (i+1)))) )
+
+
+-- unify :: Term -> Term -> Maybe Subst
+-- unify (Var x) (Var y)                       = if x /= y then Just (single x (Var y)) else Nothing
+-- unify (Var x) (Comb name term)              = if occurs x (Comb name term) then Nothing else Just (single x (Comb name term))
+-- unify (Comb name term) (Var x)              = if occurs x (Comb name term) then Nothing else Just (single x (Comb name term))
+-- unify (Comb name1 []) (Comb name2 term2)    = Nothing
+-- unify (Comb name1 term1) (Comb name2 [])    = Nothing
+-- unify (Comb name1 term1) (Comb name2 term2) = if name1 == name2 && length term1 == length term2 then Just (foldl compose empty (help term1 term2)) else Nothing 
+--   where 
+--     help :: [Term] -> [Term] -> [Subst]
+--     help (x:xs) (y:ys) = if isNothing (unify x y) then [empty] else fromJust (unify x y) : help xs ys
+--     help _ _ = []
 
 unify :: Term -> Term -> Maybe Subst
-unify t11 t21 = h1 t11 t21 (ds t11 t21) empty 
+unify t111 t211 = unifyh t111 t211 empty
   where
-    h1 :: Term -> Term -> Maybe (Term, Term) -> Subst -> Maybe Subst
-    h1 (Var x) (Var y) (Just (t3, t4)) sub = if t3 /= t4 then Just (compose sub (single x (Var y))) else Nothing
-    h1 (Comb st1 t1) (Var y) (Just (t3,t4)) sub = if y `elem` allVars (Comb st1 t1) then Nothing else Just (compose sub (single y (Comb st1 t1)))
-    h1 (Var x) (Comb st2 t2) (Just (t3,t4)) sub = if x `elem` allVars (Comb st2 t2) then Nothing else Just (compose sub (single x (Comb st2 t2)))
-    h1 (Comb st1 []) (Comb st2 t2) _ _ = Nothing
-    h1 (Comb st1 t1) (Comb st2 []) _ _ = Nothing
-    h1 (Comb st1 (t1:tx)) (Comb st2 (t2:ty)) (Just (t3, t4)) sub = if st1 /= st2 then Nothing else if (ds t1 t2 /= Nothing) then Just (compose sub (compose (notMaybe(h1 t1 t2 (ds t1 t2) sub))
-                                                                   (notMaybe(h1 (Comb st1 tx) (Comb st2 ty) (ds (head tx) (head ty)) sub)) ) ) else Nothing
-    h1 _ _ _ _ = Nothing
-    notMaybe :: Maybe Subst -> Subst
-    notMaybe (Just s1) = s1
-    notMaybe Nothing = empty
+    unifyh :: Term -> Term -> Subst -> Maybe Subst
+    unifyh (Var x) (Var y) akku = if ds (Var x) (Var y) == Nothing then Just (compose akku (single x (Var y))) else Nothing
+    unifyh (Var x) (Comb st2 t2) akku = if occurs x (Comb st2 t2) then Nothing else Just (single x (Comb st2 t2))
+    unifyh (Comb st1 t1) (Var y) akku = if occurs y (Comb st1 t1) then Nothing else Just (single y (Comb st1 t1))
+    unifyh (Comb st1 t1) (Comb st2 t2) akku
+      | ds (Comb st1 t1) (Comb st2 t2) == Nothing = Just akku
+      | isVar (fst (fromJust (ds (apply akku (Comb st1 t1)) (apply akku (Comb st2 t2))))) && helpOcc (Comb st1 t1) (Comb st2 t2) = Just (help (Comb st1 t1) (Comb st2 t2) akku)
+      | otherwise = Nothing
+
+help :: Term -> Term -> Subst -> Subst
+help t1 t2 sub = if isVar (fst (fromJust (ds (apply sub t1) (apply sub t2)))) && helpOcc t1 t2 then compose sub (mkSub t1 t2) else empty --richtige Reihenfolge yay :D
+
+mkSub :: Term -> Term -> Subst
+mkSub (Var x) t2 = single x t2
+mkSub t1 _ = empty
+
+helpOcc :: Term -> Term -> Bool
+helpOcc (Var x) t2 = occurs x t2
+helpOcc t1 _ = False
+
+isVar :: Term -> Bool
+isVar (Var x) = True
+isVar _ = False
+
+-- isDsEmpty :: Term -> Term -> Subst -> Bool
+-- isDsEmpty 
+-- isDsEmpty (t1:ts1) (t2:ts2) sub = (ds (apply sub t1) (apply sub t2)) == [] && isDsEmpty ts1 ts2
+
 
 -- Run all tests
 return []
 testUnification :: IO Bool
-testUnification = $verboseCheckAll
+testUnification = $quickCheckAll
